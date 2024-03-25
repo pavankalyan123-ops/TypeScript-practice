9da13071d434722b0784bf719d6ceaca2a83c1a7
Pavan Kalyan <pavankalyannidamanuri@gmail.com>
2024-03-25 15:51:23 +0530

    showed users from API

--- rtk-example/src/components/UsersApi.tsx
+++ rtk-example/src/components/UsersApi.tsx
@@ -1,19 +1,25 @@
-import React,{useEffect} from 'react';
+import React, { useEffect } from "react";
 
-import { fetchApi } from '../redux/UsersApiSlice';
-import { useAppDispatch,useAppSelector } from '../redux/hooks';
-
+import { fetchApi } from "../redux/UsersApiSlice";
+import { useAppDispatch, useAppSelector } from "../redux/hooks";
 
 const UsersApi = () => {
-    const users=useAppSelector(state=>state.user1.users);
-    const dispatch=useAppDispatch();
-    console.log(users);
-    useEffect(()=>{
-        dispatch(fetchApi())
-    },[])
+  const users = useAppSelector((state) => state.user1.users);
+  const dispatch = useAppDispatch();
+  console.log(users);
+  useEffect(() => {
+    dispatch(fetchApi());
+  }, []);
   return (
-    <div>UsersApi</div>
-  )
-}
+    <div>
+      <h3>Users API component</h3>
+      {users.map((user) => (
+        <div>
+          <p>{user.name}</p>
+        </div>
+      ))}
+    </div>
+  );
+};
 
-export default UsersApi
+export default UsersApi;
